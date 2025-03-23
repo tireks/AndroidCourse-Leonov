8f38aa92a6b4d139378ef53c3295fada105915a8
tireks <33616425+tireks@users.noreply.github.com>
2025-03-23 15:42:59 +0700

    navigation fix
    дошлифовал навигацию, и внес правки для переключения тем

--- app/src/main/java/com/tirexmurina/facevolume/MainActivity.kt
+++ app/src/main/java/com/tirexmurina/facevolume/MainActivity.kt
@@ -3,11 +3,16 @@ package com.tirexmurina.facevolume
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
@@ -21,7 +26,10 @@ import com.tirexmurina.facevolume.features.mainScreen.MainScreen
 import com.tirexmurina.facevolume.features.searchScreen.SearchScreen
 import com.tirexmurina.facevolume.features.settingsScreen.SettingsScreen
 import com.tirexmurina.facevolume.ui.theme.FaceVolumeTheme
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,57 +49,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FaceVolumeApp() {
-    val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = "main"){
-        composable("main") {
-            MainScreen(
-                // При клике на контакт переходим на InfoScreen или EditScreen,
-                // например:
-                onItemClick = { contactId -> navController.navigate("info/$contactId") },
-                onSettingsClick = { navController.navigate("settings") },
-                onSearchClick = { navController.navigate("search") },
-                onAddClick = { /* обработка добавления нового контакта */ }
-            )
-        }
-        composable(
-            "info/{contactId}",
-            arguments = listOf(navArgument("contactId") { type = NavType.LongType })
-        ) { backStackEntry ->
-            val contactId = backStackEntry.arguments?.getLong("contactId")
-            InfoScreen(
-                onBackClick = { navController.popBackStack() },
-                onEditClick = { id -> navController.navigate("edit/${id}") },
-                contactId = contactId
-            )
-        }
-        composable(
-            "edit/{contactId}",
-            arguments = listOf(navArgument("contactId") {
-                type = NavType.LongType
-                nullable = true
-                defaultValue = null
-            })
-        ) { backStackEntry ->
-            val contactId = backStackEntry.arguments?.getLong("contactId")
-            // Здесь contactId может быть null, что означает создание нового контакта.
-            EditScreen(
-                onBackClick = { navController.popBackStack() },
-                contactId = contactId
-            )
-        }
-        composable("search") {
-            SearchScreen(
-                onBackClick = { navController.popBackStack() },
-                onItemClick = { contactId -> navController.navigate("info/$contactId") },
-            )
-        }
-        composable("settings") {
-            SettingsScreen(
-                onBackClick = { navController.popBackStack() },
-                onModeSwitchClick = { /*TODO*/ }
-            )
+    val systemDarkTheme = isSystemInDarkTheme()
+    var darkThemeEnabled by rememberSaveable { mutableStateOf(systemDarkTheme) }
+
+    FaceVolumeTheme(darkTheme = darkThemeEnabled) {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = "main"){
+            composable("main") {
+                MainScreen(
+                    onItemClick = { contactId -> navController.navigate("info/$contactId") },
+                    onSettingsClick = { navController.navigate("settings") },
+                    onSearchClick = { navController.navigate("search") },
+                    onAddClick = { navController.navigate("edit/-1") }
+                )
+            }
+            composable(
+                "info/{contactId}",
+                arguments = listOf(navArgument("contactId") { type = NavType.LongType })
+            ) { backStackEntry ->
+                val contactId = backStackEntry.arguments?.getLong("contactId")
+                InfoScreen(
+                    onBackClick = { navController.popBackStack() },
+                    onEditClick = { id -> navController.navigate("edit/${id}") },
+                    contactId = contactId
+                )
+            }
+            composable(
+                "edit/{contactId}",
+                arguments = listOf(navArgument("contactId") {
+                    type = NavType.LongType
+                    defaultValue = -1L
+                })
+            ) { backStackEntry ->
+                val contactId = backStackEntry.arguments?.getLong("contactId")
+                EditScreen(
+                    onBackClick = {
+                        val previousRoute = navController.previousBackStackEntry?.destination?.route
+                        if (previousRoute?.startsWith("info/") == true) {
+                            navController.popBackStack()  // удаляем EditScreen
+                            navController.popBackStack()  // удаляем старый InfoScreen
+                            navController.navigate("info/$contactId")
+                        } else {
+                            navController.popBackStack()
+                        }
+                    },
+                    contactId = contactId
+                )
+            }
+            composable("search") {
+                SearchScreen(
+                    onBackClick = { navController.popBackStack() },
+                    onItemClick = { contactId -> navController.navigate("info/$contactId") },
+                )
+            }
+            composable("settings") {
+                SettingsScreen(
+                    onBackClick = { navController.popBackStack() },
+                    isDarkThemeEnabled = darkThemeEnabled,
+                    onModeSwitchClick = { darkThemeEnabled = it }
+                )
+            }
         }
     }
 }
