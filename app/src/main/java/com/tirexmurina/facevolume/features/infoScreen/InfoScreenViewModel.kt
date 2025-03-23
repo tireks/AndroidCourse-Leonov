36111aa0af9f880a5dd86e244774d6950416afbb
tireks <33616425+tireks@users.noreply.github.com>
2025-03-23 15:39:25 +0700

    info screen mvvm rework (+ ui fixes)
    В рамках этого коммита добавлены состояния для экрана, написана и подключена вьюмодель, а так же реализовано управление визуалом от состояний с вьюмодели

--- app/src/main/java/com/tirexmurina/facevolume/features/infoScreen/InfoScreenViewModel.kt
+++ app/src/main/java/com/tirexmurina/facevolume/features/infoScreen/InfoScreenViewModel.kt
@@ -1,6 +1,50 @@
 package com.tirexmurina.facevolume.features.infoScreen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tirexmurina.facevolume.shared.domain.usecase.GetContactByIdUseCase
+import com.tirexmurina.facevolume.shared.util.NoSuchElementException
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class InfoScreenViewModel() : ViewModel() {
+@HiltViewModel
+class InfoScreenViewModel @Inject constructor(
+    private val getContactByIdUseCase: GetContactByIdUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<InfoScreenState>(InfoScreenState.Initial)
+    val uiState: StateFlow<InfoScreenState> = _uiState.asStateFlow()
+
+    fun getContact(id : Long?) {
+        _uiState.value = InfoScreenState.Loading
+        if (id == null || id < 0) {
+            errorHandler(NoSuchElementException())
+        } else {
+            viewModelScope.launch {
+                try {
+                    val result = getContactByIdUseCase(id)
+                    _uiState.value = InfoScreenState.Success(result)
+                } catch (e : Exception) {
+                    errorHandler(e)
+                }
+            }
+        }
+
+    }
+
+    private fun errorHandler(exception: Exception) {
+        when(exception) {
+            is NoSuchElementException -> {
+                _uiState.value = InfoScreenState.Error("Контакт не найден")
+            }
+
+            else -> {
+                _uiState.value = InfoScreenState.Error("Неизвестная ошибка")
+            }
+        }
+    }
 }
