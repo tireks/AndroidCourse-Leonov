7f1e618c50b3c1ffd3f048190f59fd8fc0167a78
tireks <33616425+tireks@users.noreply.github.com>
2025-03-23 23:13:01 +0700

    move
    решил перенести файлик

--- app/src/main/java/com/tirexmurina/facevolume/shared/util/ContactConvertHelper.kt
+++ app/src/main/java/com/tirexmurina/facevolume/shared/util/ContactConvertHelper.kt
@@ -1,5 +1,8 @@
-package com.tirexmurina.facevolume.shared.data.local.model
+package com.tirexmurina.facevolume.shared.util
 
+import com.tirexmurina.facevolume.shared.data.local.model.ContactLocalDatabaseModel
+import com.tirexmurina.facevolume.shared.data.local.model.LocationLocalDatabaseModel
+import com.tirexmurina.facevolume.shared.data.remote.model.RandomContact
 import com.tirexmurina.facevolume.shared.domain.entity.Contact
 
 fun Contact.toLocalDatabaseModel(): ContactLocalDatabaseModel {
@@ -37,5 +40,22 @@ fun ContactLocalDatabaseModel.toDomainModel(): Contact {
                 timezone = location.timezone
             )
         }
+    )
+}
+
+fun RandomContact.toDomainModel(): Contact {
+    return Contact(
+        id = -1L,
+        name = "${this.name.first} ${this.name.last}",
+        pic = this.picture.large,
+        phone = this.phone,
+        email = this.email,
+        location = Contact.Location(
+            country = this.location.country,
+            city = this.location.city,
+            address = "${this.location.street.number} ${this.location.street.name}",
+            timezone = Contact.Timezone.entries.toTypedArray().random()
+        ),
+        note = null
     )
 }
