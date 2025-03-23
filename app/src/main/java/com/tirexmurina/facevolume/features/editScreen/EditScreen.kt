86bd8a0b1724cc3069495afc4cf540caba6549af
tireks <33616425+tireks@users.noreply.github.com>
2025-03-23 15:39:15 +0700

    edit screen mvvm rework (+ ui fixes)
    В рамках этого коммита добавлены состояния для экрана, написана и подключена вьюмодель, а так же реализовано управление визуалом от состояний с вьюмодели

--- app/src/main/java/com/tirexmurina/facevolume/features/editScreen/EditScreen.kt
+++ app/src/main/java/com/tirexmurina/facevolume/features/editScreen/EditScreen.kt
@@ -1,7 +1,16 @@
 package com.tirexmurina.facevolume.features.editScreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.tirexmurina.facevolume.features.infoScreen.InfoScreenState
 import com.tirexmurina.facevolume.features.infoScreen.InfoScreenViewModel
 
 @Composable
@@ -10,9 +19,29 @@ fun EditScreen(
     onBackClick:() -> Unit,
     contactId : Long?
 ){
-    /*val viewState by viewModel.uiState.collectAsState()
+    val viewState by viewModel.uiState.collectAsState()
 
     when (viewState){
+        EditScreenState.Initial -> viewModel.getContact(contactId)
 
-    }*/
+        EditScreenState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        is EditScreenState.Success -> {
+            EditScreenContent(
+                onBackClick = onBackClick,
+                contact = (viewState as EditScreenState.Success).contact,
+                onSave = { viewModel.saveContact(it) }
+            )
+        }
+
+        is EditScreenState.Error -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = (viewState as EditScreenState.Error).msg)
+            }
+        }
+    }
 }
