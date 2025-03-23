93c4c4a78406691ef29b9a9eec64b660e9a176e6
tireks <33616425+tireks@users.noreply.github.com>
2025-03-23 23:12:37 +0700

    repo fix

--- app/src/main/java/com/tirexmurina/facevolume/shared/data/ContactRepositoryImpl.kt
+++ app/src/main/java/com/tirexmurina/facevolume/shared/data/ContactRepositoryImpl.kt
@@ -1,18 +1,20 @@
 package com.tirexmurina.facevolume.shared.data
 
 import com.tirexmurina.facevolume.shared.data.local.ContactDao
-import com.tirexmurina.facevolume.shared.data.local.FakeContacts
-import com.tirexmurina.facevolume.shared.data.local.model.toDomainModel
-import com.tirexmurina.facevolume.shared.data.local.model.toLocalDatabaseModel
+import com.tirexmurina.facevolume.shared.util.toDomainModel
+import com.tirexmurina.facevolume.shared.util.toLocalDatabaseModel
+import com.tirexmurina.facevolume.shared.data.remote.ContactService
 import com.tirexmurina.facevolume.shared.domain.entity.Contact
 import com.tirexmurina.facevolume.shared.domain.repository.ContactRepository
 import com.tirexmurina.facevolume.shared.util.ContactListCorruptedException
 import com.tirexmurina.facevolume.shared.util.ContactSavingException
 import com.tirexmurina.facevolume.shared.util.NoSuchElementException
+import com.tirexmurina.facevolume.shared.util.RandomContactException
 import javax.inject.Inject
 
 class ContactRepositoryImpl @Inject constructor(
-    private val contactDao: ContactDao
+    private val contactDao: ContactDao,
+    private val contactService: ContactService
 ) : ContactRepository {
     override suspend fun getContacts(): List<Contact> {
         return try {
@@ -43,6 +45,13 @@ class ContactRepositoryImpl @Inject constructor(
         }
     }
 
+    override suspend fun getRandomContact(): Contact {
+        return try {
+            contactService.getRandomUser().results[0].toDomainModel()
+        }  catch ( e : Exception) {
+            throw RandomContactException("Failed to get random contact")
+        }
+    }
 
 
 }
